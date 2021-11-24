3409e684c31835cb57110b3a6e4735b31bd90e25
Cmalaguita <72543773+Cmalaguita@users.noreply.github.com>
2021-11-24 23:00:55 +0100

    codigo del navigation drawer documentado y limpiado

--- ejerciciocomponentes/lib/navigation_drawer/navigation_drawer.dart
+++ ejerciciocomponentes/lib/navigation_drawer/navigation_drawer.dart
@@ -4,55 +4,18 @@ import 'package:ejerciciocomponentes/src/utils/icon_utils.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+///esta clase contiene un drawer que recibe una lista de paginas provenientes de un json desde las que el usuario puede acceder al resto de paginas de la aplicacion, salvo al login y al registro.
 class navigationDrawer extends StatelessWidget {
   final List<String> opciones = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinque'];
 
   @override
   Widget build(BuildContext context) {
     return Drawer(
-        // Add a ListView to the drawer. This ensures the user can scroll
-        // through the options in the drawer if there isn't enough vertical
-        // space to fit everything.
-        // child:_lista(),
-        
-        
-        child: ListView(
-      scrollDirection: Axis.vertical,
-      shrinkWrap: true,
-      children: [
-        UserAccountsDrawerHeader(
-          accountName: Text('Bahamadia'),
-          accountEmail: Text('Bahamadia.og@gmail.com'),
-          currentAccountPicture: CircleAvatar(
-            child: ClipOval(
-              child: Image.network(
-                'https://learn.zoner.com/wp-content/uploads/2015/06/028mm.jpg?fidl=2019-06-mag-en',
-                fit: BoxFit.cover,
-                width: 70,
-                height: 80,
-              ),
-            ),
-          ),
-          decoration: BoxDecoration(
-              color: Colors.blue.shade500,
-              image: const DecorationImage(
-                fit: BoxFit.fill,
-                image: NetworkImage(
-                    'https://static.vecteezy.com/system/resources/previews/001/217/366/non_2x/polygonal-blue-background-vector.jpg'),
-              )),
-            
-        ),
-Container(
-
-child: const Text('Polla'),
-
-)
-      ],
-    )
-    
+       
+        child:_lista(),
     );
   }
-
+/// metodo que devuelve un futurebuilder con una listview que contiene las direcciones de paginas desde las que el usuario accederá al drawer
   Widget _lista() {
     return FutureBuilder(
         future: menuProvider.cargarData(),
@@ -63,7 +26,7 @@ child: const Text('Polla'),
           );
         });
   }
-
+  /// metodo que retorna una lista de listTiles que obtiene a traves del menuprovider del metodo _lista
   List<Widget> _listaItems(List<dynamic>? data, BuildContext context) {
     final List<Widget> opciones = [];
     data?.forEach((item) {
@@ -79,25 +42,5 @@ child: const Text('Polla'),
       opciones.add(Divider());
     });
     return opciones;
-  }
-
-  List<Widget> _crearItemsCorto(context) {
-    return opciones.map((opcion) {
-      return Column(
-        children: [
-          ListTile(
-            title: Text(opcion),
-            subtitle: Text('hola tete'),
-            leading: Icon(Icons.account_box_rounded),
-            trailing: Icon(Icons.arrow_right),
-            onTap: () {
-              final ruta = MaterialPageRoute(builder: (context) => AlertPage());
-              Navigator.push(context, ruta);
-            },
-          ),
-          Divider()
-        ],
-      );
-    }).toList();
   }
 }
